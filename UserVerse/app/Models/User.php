57c576dc492be16d962571c77a0597262924d8a7
Youssef BAHI <you97ssef@gmail.com>
2023-11-03 19:20:08 +0100

    Add soft deletes to User model and define user roles as constants.

--- UserVerse/app/Models/User.php
+++ UserVerse/app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Lumen\Auth\Authorizable;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, Authorizable, HasFactory;
+    use Authenticatable, Authorizable, HasFactory, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -30,4 +31,9 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+
+    const USER = 0;
+    const SHIPPER = 1;
+    const ADMIN = 2;
 }
