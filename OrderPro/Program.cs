7556e1f98131b8cb273a31c6be97821fc45c77a4
Youssef BAHI <you97ssef@gmail.com>
2023-11-04 16:30:41 +0100

    Add connection string and configure OrderProContext

--- OrderPro/Program.cs
+++ OrderPro/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using OrderPro.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<OrderProContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("OrderProContext")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
