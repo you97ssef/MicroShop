bc4172fd0ab0620e517a742d6d4a7cef0c4f4514
Youssef BAHI <you97ssef@gmail.com>
2023-11-04 17:15:06 +0100

    Add checkout functionality to Cart model

---
+++ OrderPro/Data/Migrations/20231104155713_AddedCheckoutToCart.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderPro.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCheckoutToCart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CheckedOut",
+                table: "Carts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckedOut",
+                table: "Carts");
+        }
+    }
+}
--- OrderPro/Models/Cart.cs
+++ OrderPro/Models/Cart.cs
@@ -3,6 +3,7 @@ public class Cart
 {
     public int Id { get; set; }
     public int UserId { get; set; }
+    public bool CheckedOut { get; set; } = false;
 
     public List<Item>? Items { get; set; }
 }
