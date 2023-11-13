a0a8ed4dcf0f6a8da9a80fbd8dc6598ce6649a47
Youssef BAHI <you97ssef@gmail.com>
2023-11-13 14:23:18 +0100

    Add new routes for payments and shipments

--- PayShip/routes/api.js
+++ PayShip/routes/api.js
@@ -10,10 +10,14 @@ router.post("/test", testController.post);
 const paymentController = require("../controllers/paymentController.js");
 
 router.post("/payment", paymentController.makePayment);
+router.get("/payment", paymentController.payments);
+router.get("/payment/:orderId", paymentController.order);
 
 const shippingController = require("../controllers/shippingController.js");
 
 router.get("/shipping/:id", shippingController.checkStatus);
 router.put("/shipping/:id", shippingController.changeStatus);
+router.get("/shipping", shippingController.shipments);
+router.get("/order-shipping/:orderId", shippingController.order);
 
 module.exports = router;
