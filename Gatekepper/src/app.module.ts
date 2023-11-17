402be4eed9ffa3ab6b7ef78258e07acb084fdc04
Youssef BAHI <you97ssef@gmail.com>
2023-11-17 22:13:52 +0100

    Add UserVerse Service

--- Gatekepper/src/app.module.ts
+++ Gatekepper/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TestController } from './controllers/test.controller';
 import { TestService } from './services/test.service';
+import { UserVerseService } from './services/user-verse.service';
 
 @Module({
     imports: [],
     controllers: [TestController],
-    providers: [TestService],
+    providers: [TestService, UserVerseService],
 })
 export class AppModule {}
