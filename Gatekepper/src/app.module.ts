9fd3e70a092dae23b6dc668f7cf02950c781790a
Youssef BAHI <you97ssef@gmail.com>
2023-11-19 01:23:56 +0100

    Add USER_VERSE environment variables and UserController

--- Gatekepper/src/app.module.ts
+++ Gatekepper/src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { TestController } from './controllers/test.controller';
 import { TestService } from './services/test.service';
 import { UserVerseService } from './services/user-verse.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { UserController } from './controllers/user.controller';
 
 @Module({
-    imports: [],
-    controllers: [TestController],
+    imports: [
+        HttpModule, 
+        ConfigModule.forRoot({
+            envFilePath: '.env.development',
+        })
+    ],
+    controllers: [TestController, UserController],
     providers: [TestService, UserVerseService],
 })
 export class AppModule {}
