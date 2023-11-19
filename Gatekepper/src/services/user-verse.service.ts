8f521cd2dddac0096a625c0a05c0bf1e34bd0c74
Youssef BAHI <you97ssef@gmail.com>
2023-11-19 01:29:31 +0100

    Finish UserVerse service

--- Gatekepper/src/services/user-verse.service.ts
+++ Gatekepper/src/services/user-verse.service.ts
@@ -1,18 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, HttpException, Logger } from '@nestjs/common';
+import { AxiosResponse, AxiosError } from 'axios';
 import { LoginDto, RegisterDto } from 'src/dtos/user.dto';
 import { UserRole } from 'src/helpers/user.helper';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class UserVerseService {
-    login(login: LoginDto) {
+    private readonly url: string = process.env.USER_VERSE;
+    private readonly logger = new Logger(UserVerseService.name);
 
+    constructor(private readonly api: HttpService) { 
+        api.axiosRef.interceptors.response.use(
+            (response: AxiosResponse) => {
+                return response.data;
+            },
+            (error: AxiosError) => {
+                this.logger.error(error);
+                throw new HttpException(error.response.data, error.response?.status);
+            },
+        );
     }
 
-    register(register: RegisterDto) {
-
+    async login(login: LoginDto) {
+        return await firstValueFrom(this.api.post(`${this.url}/login`, login));
     }
 
-    verify(role: UserRole) {
+    async register(register: RegisterDto) {
+        return await firstValueFrom(this.api.post(`${this.url}/register`, register));
+    }
 
+    async verify(role: UserRole, token: string) {
+        return await firstValueFrom(this.api.get(`${this.url}/${role}`, {
+            headers: {
+                Authorization: token,
+            },
+        }));
     }
 }
