7ab12fd6ded0f83b0ced0e01400ef7c62d27d7cf
Youssef BAHI <you97ssef@gmail.com>
2023-11-21 00:48:08 +0100

    Add role-based guards for user authentication

---
+++ Gatekepper/src/guards/role.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { UserRole } from 'src/helpers/user-verse.helper';
+import { UserVerseService } from 'src/services/user-verse.service';
+
+class RoleGuard implements CanActivate {
+    constructor(
+        private role: UserRole,
+        private readonly userVerseService: UserVerseService,
+    ) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const response = await this.userVerseService.verify(this.role, request.headers.authorization);
+        request.user = response;
+        return true;
+    }
+}
+
+@Injectable()
+export class AdminGuard extends RoleGuard {
+    constructor(userVerseService: UserVerseService) {
+        super(UserRole.ADMIN, userVerseService);
+    }
+}
+
+@Injectable()
+export class ShipperGuard extends RoleGuard {
+    constructor(userVerseService: UserVerseService) {
+        super(UserRole.SHIPPER, userVerseService);
+    }
+}
+
+@Injectable()
+export class UserGuard extends RoleGuard {
+    constructor(userVerseService: UserVerseService) {
+        super(UserRole.USER, userVerseService);
+    }
+}
